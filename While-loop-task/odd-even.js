640cd15ad2af130b402c1fdd102b2eaeb6c37f0e
codewithalif <programmingwithalif@gmail.com>
2026-02-19 20:38:47 +0600

    while loop odd and even task done

---
+++ While-loop-task/odd-even.js
@@ -0,0 +1,16 @@
+// even number
+
+let even = 78;
+while(even <=98){
+  console.log(even);
+  even = even + 2;
+}
+
+// odd number
+
+let odd = 61;
+while(odd <= 100){
+  console.log(odd);
+  odd = odd + 2;
+}
+
