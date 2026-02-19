7c6d14c2174e3a110b004a7391d9fbc142bb010c
codewithalif <programmingwithalif@gmail.com>
2026-02-19 12:22:34 +0600

    3 sum of odd numbers- task done

---
+++ For-loop-task/3-sum-of-odd-numbers.js
@@ -0,0 +1,18 @@
+// Subtask 1
+
+//even number
+let sum = 0;
+for(i = 51 ; i <= 85 ; i=i+2){
+  sum = sum + i;
+  console.log(i);
+}
+console.log('sum of all the even numbers from 51 to 85 is ', sum)
+// Subtask 2
+
+ //odd number
+let num = 0;
+for(i = 91 ; i <= 129 ; i=i+2){
+  num = num + i;
+  console.log(i);
+}
+console.log('sum of all the odd numbers from 91 to 129 is ', num)
